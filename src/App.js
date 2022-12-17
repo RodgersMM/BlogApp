1b21b463a1e142f7dc7a9df92219569675e76b33
RodgersMM <rodgersmutua44@gmail.com>
2022-12-17 12:27:46 +0300

    menu and header

---
+++ src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+import TopBar from './topbar/TopBar';
+import Home from './pages/home/Home';
+import Header from './header/Header';
+
+function App() {
+  return (
+    <> 
+      <TopBar />
+      <Header />
+     
+      
+       
+       
+    </>
+    
+  );
+  
+}
+
+export default App;
