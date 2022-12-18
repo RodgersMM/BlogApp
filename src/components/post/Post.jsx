2585ee3d25c92c68b21ef27b876066a4fd7c1d46
RodgersMM <rodgersmutua44@gmail.com>
2022-12-18 17:50:02 +0300

    Adding sample articles and sidebar

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,54 @@
+import React from 'react'
+import './post.css'
+
+
+function Post() {
+  return (
+    <div className='post'>
+           <img className='postImg' 
+        src='https://www.pexels.com/photo/woman-jumping-wearing-green-backpack-214574/'
+        alt="Maldive Beach"
+        />
+   
+        <div className="postInfo">
+          <div className="postCats">
+            <span className="postCat">Core</span>
+            <span className="postCat">RAN</span>
+        </div>
+          <span className="postTitle">Lorem ipsum dolor, sit amet adipisicing elit</span>
+         <hr/>
+         <span className="postDate">1 hour ago</span>
+         <p className='postDescr'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+             
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+             
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+
+
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+
+             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nisi perspiciatis nihil placeat doloribus dolorem deleniti minus voluptatibus labore nam tenetur velit aliquam vel,
+             rerum voluptatem similique incidunt atque, minima numquam?
+             </p>
+
+
+    </div>
+ </div>
+    
+  );
+}
+
+export default Post
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -3,12 +3,12 @@ import './sidebar.css'
 function Sidebar() {
   return (
     <div className='sidebar'>
-       <div className="sideBarItem">
-        <span className="sidebarTitle">ABOUT ME        </span>
-        <p>Full Stack Developer</p>
+       <div className="sidebarItem">
+        <span className="sidebarTitle">ABOUT ME</span>
+        <p>I'm a Full Stack Developer & Digital Marketer</p>
        </div>
        <div className="sidebarItem">
-         <span>CATEGORIES</span>
+         <span className="sidebarTitle">CATEGORIES</span>
           <ul className="sidebarList">
             <li className="sidebarListItem">Life</li>
             <li className="sidebarListItem">National</li>
@@ -22,7 +22,6 @@ function Sidebar() {
           <div className="sidebaSocial">
              <i className="sidebarIcon fa-brands fa-facebook"></i>
              <i className="sidebarIcon fa-brands fa-twitter"></i>
-
           </div>
         </div>
     </div>
